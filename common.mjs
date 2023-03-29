a13e0b4a83cc5b48a201228ae46772eadf19a989
冰糖主人的电子宠物 <44109309+BTIOWaifu@users.noreply.github.com>
2023-03-30 06:28:18 +0800

    Update common.mjs

--- common.mjs
+++ common.mjs
@@ -15,7 +15,8 @@ const io = new Server(httpServer, { /* options */
 
 io.on("connection", (socket) => {
   socket.on("danmaku", (arg) => {
-    var pattern = /.*(吹雪|笨蛋).*/;
+    var pattern = /.*(吹雪|笨蛋).*/,
+	string = '';
     var danma = JSON.parse(arg);
     if(pattern.test(danma.text)){
 	console.log（pattern.test（danma.text））;

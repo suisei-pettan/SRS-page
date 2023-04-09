674b2dcb73364efc17d3feafd418d7021aa14c8e
冰糖主人的电子宠物 <44109309+BTIOWaifu@users.noreply.github.com>
2023-04-09 18:28:40 +0800

    添加ua

--- gitlink.py
+++ gitlink.py
@@ -10,7 +10,7 @@ def get_video_url(video_url):
 
 def get_ffmpeg_command(video_url, ch):
     url = get_video_url(video_url)
-    command = f'ffmpeg -loglevel quiet -i {url} -c:v copy -strict -2 -f flv rtmp://127.0.0.1/live/homo{ch}.flv'
+    command = f'ffmpeg -loglevel quiet -i {url} -c:v copy -strict -2 -user_agent "Mozilla/5.0 (Linux; Android 13; 21051182C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 EdgA/111.0.1661.59" -f flv rtmp://127.0.0.1/live/homo{ch}.flv'
     return command
     
 video_url = input("请输入YouTube视频链接：")

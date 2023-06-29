ce5cb853694234ca8dd790b1112766966ec8f413
Sui酱今天也很小 <44109309+suisei-pettan@users.noreply.github.com>
2023-06-29 18:50:24 +0800

    更新 yt-dlp 名

--- stream.py
+++ stream.py
@@ -7,7 +7,7 @@ def main():
     channel_number = sys.argv[2]
 
     # 执行 ytdl-patched -g youtube视频链接 获取其输出的链接存入 源直播 变量
-    ytdl_patched_output = subprocess.check_output(f"ytdl-patched -g {youtube_url}", shell=True)
+    ytdl_patched_output = subprocess.check_output(f"yt-dlp -g {youtube_url}", shell=True)
     source_live = ytdl_patched_output.decode().strip()
     # 执行 ffmpeg -loglevel quiet -i 源直播 -c:v copy -strict -2 -f flv rtmp://127.0.0.1/live/homo频道号.flv
     ffmpeg_command = f"ffmpeg -loglevel quiet -i {source_live} -c:v copy -strict -2 -f flv rtmp://127.0.0.1/live/homo{channel_number}.flv"
